e4f186298c6805c76be184fac3faa1c6f415964d
Sergio <segio91@gmail.com>
2015-11-24 20:09:00 +0100

    comiteo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -32,6 +32,6 @@
  <p>Comentario: {{ $image->comment }}</p>
  <hr/>
 @endforeach
-<p>nuevo commit lalalalalalalala</p>
+<p>nuevo commit dris dros</p>
 <img class="capa" src="{{ asset('canvas/56538d38c89e0.png') }}"/>
 <img class="capa" src="{{ asset('canvas/56538eafaeb75.png') }}"/>
